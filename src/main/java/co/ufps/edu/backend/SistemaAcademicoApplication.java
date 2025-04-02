928c245133aad9a7e30f20d63521fce2542e4aba
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-04-01 22:38:47 -0500

    Initialize project with base structure

---
+++ src/main/java/co/ufps/edu/backend/SistemaAcademicoApplication.java
@@ -0,0 +1,13 @@
+package co.ufps.edu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademicoApplication.class, args);
+	}
+
+}
