1f5eab25ac84152615973e9cf736aa3d2e6edda2
Diego_jt <99993658+Diegoj17@users.noreply.github.com>
2025-04-11 14:27:39 -0500

    Cambios en el model Asignatura

--- src/main/java/co/ufps/edu/backend/model/Asignatura.java
+++ src/main/java/co/ufps/edu/backend/model/Asignatura.java
@@ -4,16 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Objects;
 
 @Entity
-@Table(name = "asignaturas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Asignatura {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int codigo;
+    private long id;
+
+    private String codigo;
 
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
@@ -24,67 +31,4 @@ public class Asignatura {
     @Positive(message = "Los créditos deben ser un valor positivo")
     private byte creditos;
 
-    public Asignatura() {
-    }
-
-    public Asignatura(int codigo, String nombre, String descripcion, byte creditos) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.creditos = creditos;
-    }
-
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public byte getCreditos() {
-        return creditos;
-    }
-
-    public void setCreditos(byte creditos) {
-        this.creditos = creditos;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Asignatura that = (Asignatura) o;
-        return codigo == that.codigo && creditos == that.creditos && Objects.equals(nombre, that.nombre) && Objects.equals(descripcion, that.descripcion);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(codigo, nombre, descripcion, creditos);
-    }
-
-    @Override
-    public String toString() {
-        return "Asignatura{" +
-                "codigo=" + codigo +
-                ", nombre='" + nombre + '\'' +
-                ", descripcion='" + descripcion + '\'' +
-                ", creditos=" + creditos +
-                '}';
-    }
 }
