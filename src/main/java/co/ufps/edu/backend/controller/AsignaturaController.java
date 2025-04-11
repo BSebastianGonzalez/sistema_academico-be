8c31b7d5393f044747cae7bd37eb2a8ada83bda0
Diego_jt <99993658+Diegoj17@users.noreply.github.com>
2025-04-11 15:25:08 -0500

    Cambios en el controller Asignatura

--- src/main/java/co/ufps/edu/backend/controller/AsignaturaController.java
+++ src/main/java/co/ufps/edu/backend/controller/AsignaturaController.java
@@ -5,6 +5,7 @@ import co.ufps.edu.backend.model.Asignatura;
 import co.ufps.edu.backend.repository.AsignaturaRepository;
 import co.ufps.edu.backend.service.AsignaturaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,36 +18,67 @@ public class AsignaturaController {
 
     private final AsignaturaService asignaturaService;
 
+    //Listar Asignaturas
     @GetMapping
     public List<Asignatura> obtenerTodasLasAsignaturas() {
         return asignaturaService.obtenerTodasLasAsignaturas();
     }
 
-    @GetMapping("/{codigo}")
-    public ResponseEntity<Asignatura> obtenerAsignaturaPorCodigo(@PathVariable int codigo) {
+    //Obtener Asignatura por Id
+    @GetMapping("/{id}")
+    public ResponseEntity<Asignatura> obtenerAsignaturaPorId(@PathVariable Long id) {
+        return asignaturaService.obtenerAsignaturaPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    //Obtener Asignatura por Codigo
+    @GetMapping("/codigo/{codigo}")
+    public ResponseEntity<Asignatura> obtenerAsignaturaPorCodigo(@PathVariable String codigo) {
         return asignaturaService.obtenerAsignaturaPorCodigo(codigo)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    /*
+    @PostMapping
+    public ResponseEntity<Asignatura> crearAsignatura(@RequestBody Asignatura asignatura) {
+        Asignatura nuevaAsignatura = asignaturaService.crearAsignatura(asignatura);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaAsignatura);
+    }
+     */
+
+    // Crear Asignatura
     @PostMapping
     public Asignatura crearAsignatura(@RequestBody Asignatura asignatura) {
         return asignaturaService.crearAsignatura(asignatura);
     }
 
-    @PutMapping("/{codigo}")
-    public ResponseEntity<Asignatura> actualizarAsignatura(@PathVariable int codigo, @RequestBody Asignatura asignaturaActualizada) {
+
+    /*
+    @PostMapping
+    public ResponseEntity<Asignatura> createAsignatura(@RequestBody Asignatura asignatura) {
+        Asignatura nuevaAsignatura = asignaturaService.crearAsignatura(asignatura);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevaAsignatura);
+    }
+     */
+
+    // Actualizar Asignatura
+    @PutMapping("/{id}")
+    public ResponseEntity<Asignatura> actualizarAsignatura(@PathVariable Long id, @RequestBody Asignatura asignaturaActualizada) {
         try {
-            Asignatura actualizarAsignatura = asignaturaService.actualizarAsignatura(codigo, asignaturaActualizada);
+            Asignatura actualizarAsignatura = asignaturaService.actualizarAsignatura(id, asignaturaActualizada);
             return ResponseEntity.ok(actualizarAsignatura);
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
         }
     }
 
-    @DeleteMapping("/{codigo}")
-    public ResponseEntity<Void> eliminarAsignatura(@PathVariable int codigo) {
-        asignaturaService.eliminarAsignatura(codigo);
-        return ResponseEntity.noContent().build();
+    // Eliminar Asignatura
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarAsignatura(@PathVariable Long id) {
+        asignaturaService.eliminarAsignatura(id);
+        return ResponseEntity.notFound().build();
     }
+
 }
