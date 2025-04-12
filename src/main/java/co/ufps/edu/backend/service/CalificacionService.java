36dc83c8dcf22ea5305c282ca01e2ef78459261d
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-04-11 23:03:50 -0500

    feature: add endpoint to retrieve grades by student

--- src/main/java/co/ufps/edu/backend/service/CalificacionService.java
+++ src/main/java/co/ufps/edu/backend/service/CalificacionService.java
@@ -1,4 +1,49 @@
 package co.ufps.edu.backend.service;
 
+import co.ufps.edu.backend.model.Calificacion;
+import co.ufps.edu.backend.repository.CalificacionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CalificacionService {
+
+    @Autowired
+    private final CalificacionRepository calificacionRepository;
+
+    public List<Calificacion> getAllCalificaciones() {
+        return calificacionRepository.findAll();
+    }
+
+    public Optional<Calificacion> getCalificacionById(Long id) {
+        return calificacionRepository.findById(id);
+    }
+
+    public Calificacion createCalificacion(Calificacion calificacion) {
+        return calificacionRepository.save(calificacion);
+    }
+
+    public Calificacion updateCalificacion(Long id, Calificacion calificacionDetails) {
+        return calificacionRepository.findById(id).map(calificacion -> {
+            calificacion.setNota(calificacionDetails.getNota());
+            calificacion.setFecha(calificacionDetails.getFecha());
+            calificacion.setEstudiante(calificacionDetails.getEstudiante());
+            calificacion.setEvaluacion(calificacionDetails.getEvaluacion());
+            return calificacionRepository.save(calificacion);
+        }).orElseThrow(() -> new RuntimeException("Calificacion not found"));
+    }
+
+    public void deleteCalificacion(Long id) {
+        calificacionRepository.deleteById(id);
+    }
+
+    public List<Calificacion> getCalificacionesByEstudiante(int estudianteId) {
+        return calificacionRepository.findByEstudiante_Id(estudianteId);
+    }
 }
+
--- src/main/java/co/ufps/edu/backend/service/EvaluacionService.java
+++ src/main/java/co/ufps/edu/backend/service/EvaluacionService.java
@@ -1,4 +1,49 @@
 package co.ufps.edu.backend.service;
 
+import co.ufps.edu.backend.model.Evaluacion;
+import co.ufps.edu.backend.repository.EvaluacionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class EvaluacionService {
+
+    @Autowired
+    private final EvaluacionRepository evaluacionRepository;
+
+    public List<Evaluacion> getAllEvaluaciones() {
+        return evaluacionRepository.findAll();
+    }
+
+    public Optional<Evaluacion> getEvaluacionById(Long id) {
+        return evaluacionRepository.findById(id);
+    }
+
+    public Evaluacion createEvaluacion(Evaluacion evaluacion) {
+        return evaluacionRepository.save(evaluacion);
+    }
+
+    public Evaluacion updateEvaluacion(Long id, Evaluacion evaluacionDetails) {
+        return evaluacionRepository.findById(id).map(evaluacion -> {
+            evaluacion.setTitulo(evaluacionDetails.getTitulo());
+            evaluacion.setDescripcion(evaluacionDetails.getDescripcion());
+            evaluacion.setFechaAplicacion(evaluacionDetails.getFechaAplicacion());
+            evaluacion.setPorcentaje(evaluacionDetails.getPorcentaje());
+            evaluacion.setTipo(evaluacionDetails.getTipo());
+            evaluacion.setCreador(evaluacionDetails.getCreador());
+            evaluacion.setCurso(evaluacionDetails.getCurso());
+            evaluacion.setCalificacion(evaluacionDetails.getCalificacion());
+            return evaluacionRepository.save(evaluacion);
+        }).orElseThrow(() -> new RuntimeException("Evaluacion no encontrada"));
+    }
+
+    public void deleteEvaluacion(Long id) {
+        evaluacionRepository.deleteById(id);
+    }
 }
+
