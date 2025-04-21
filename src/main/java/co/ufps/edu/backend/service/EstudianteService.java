113463acdc84f7ceb9602bfc63c169e089ff26d9
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-04-21 17:40:14 -0500

    Actualizando creacion de Estudiante

--- src/main/java/co/ufps/edu/backend/service/EstudianteService.java
+++ src/main/java/co/ufps/edu/backend/service/EstudianteService.java
@@ -1,7 +1,12 @@
 package co.ufps.edu.backend.service;
 
+import co.ufps.edu.backend.dto.EstudianteDTO;
+import co.ufps.edu.backend.model.Carrera;
 import co.ufps.edu.backend.model.Estudiante;
+import co.ufps.edu.backend.model.Usuario;
+import co.ufps.edu.backend.repository.CarreraRepository;
 import co.ufps.edu.backend.repository.EstudianteRepository;
+import co.ufps.edu.backend.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +19,8 @@ import java.util.Optional;
 public class EstudianteService {
     @Autowired
     private final EstudianteRepository estudianteRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final CarreraRepository carreraRepository;
 
     public List<Estudiante> getAllEstudiantes() {
         return estudianteRepository.findAll();
@@ -23,20 +30,43 @@ public class EstudianteService {
         return estudianteRepository.findById(id);
     }
 
-    public Estudiante createEstudiante(Estudiante estudiante) {
-        int codigoCarrera = estudiante.getCodigoCarrera();
-        Integer ultimoId = estudianteRepository.findUltimoIdByCarrera(codigoCarrera);
-        Integer nuevoId = (ultimoId == null) ? codigoCarrera * 10000 + 1 : ultimoId + 1;
-        estudiante.setId(nuevoId);
+    public Estudiante createEstudiante(EstudianteDTO estudianteDTO) {
+        Estudiante estudiante = new Estudiante();
+
+        //Creando codigo de estudiante
+        Usuario usuario = usuarioRepository.findById(estudianteDTO.getIdUsuario())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        Carrera carrera = carreraRepository.findById(estudianteDTO.getIdCarrera())
+                .orElseThrow(() -> new RuntimeException("Carrera no encontrada"));
+        Long codCarrera = carrera.getId();
+        Long cantidadEstudiantes = estudianteRepository.countByCarrera(carrera);
+        Long id = (codCarrera * 10000) + (cantidadEstudiantes + 1);
+
+        //Creando correo
+        String apellido1 = usuario.getApellido1();
+        String apellido2 = usuario.getApellido2();
+        String nombre1 = usuario.getNombre1().toLowerCase();
+        String nombre2 = usuario.getNombre2().toLowerCase();
+
+        String correo = nombre1 + nombre2 + Character.toLowerCase(apellido1.charAt(0)) +
+                Character.toLowerCase(apellido2.charAt(0)) + "@ufps.edu.co";
+
+        estudiante.setId(id);
+        estudiante.setSemestre(estudianteDTO.getSemestre());
+        estudiante.setCreditos(0);
+        estudiante.setActivo(true);
+        estudiante.setCorreo(correo);
+        estudiante.setContrasenia(estudianteDTO.getContrasenia());
+        estudiante.setUsuario(usuario);
+        estudiante.setCarrera(carrera);
+
         return estudianteRepository.save(estudiante);
     }
 
 
     public Estudiante updateEstudiante(Long id, Estudiante estudianteDetails) {
         return estudianteRepository.findById(id).map(estudiante -> {
-            estudiante.setCodigoCarrera(estudianteDetails.getCodigoCarrera());
             estudiante.setSemestre(estudianteDetails.getSemestre());
-            estudiante.setEstado(estudianteDetails.isEstado());
             estudiante.setUsuario(estudianteDetails.getUsuario());
             return estudianteRepository.save(estudiante);
         }).orElseThrow(() -> new RuntimeException("Estudiante no encontrado"));
