a82be7ee5f7c7a2dc4c71b9bee83402e61d55bd8
Diego_jt <99993658+Diegoj17@users.noreply.github.com>
2025-04-22 16:58:43 -0500

    Se hace inscripcion

--- src/main/java/co/ufps/edu/backend/repository/InscripcionRepository.java
+++ src/main/java/co/ufps/edu/backend/repository/InscripcionRepository.java
@@ -1,7 +1,15 @@
 package co.ufps.edu.backend.repository;
 
+import co.ufps.edu.backend.model.Asignatura;
+import co.ufps.edu.backend.model.Curso;
+import co.ufps.edu.backend.model.Estudiante;
 import co.ufps.edu.backend.model.Inscripcion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
+
+    List<Inscripcion> findByEstudianteAndCurso(Estudiante estudiante, Curso curso);
+    List<Inscripcion> findByEstudianteAndCursoAsignaturaAndEstado(Estudiante estudiante, Asignatura asignatura, String estado);
 }
